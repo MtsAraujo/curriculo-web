0d9ed114b7a62cc2bc2dbff50d572dd0bb25cbd7
Matheus Araújo <MATHEUSDDSILVA@GMAIL.COM>
2022-09-21 21:42:33 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# curriculo-web
+ O portfólio foi construído para mostrar um pouco sobre meus projetos e uma breve                 descrição sobre mim. Foi Desenvolvido com HTML, CSS, JavaScript, bootstrap.
---
+++ js/habilidades.js
@@ -0,0 +1,38 @@
+const skills = document.querySelectorAll('.skill-box');
+const descricao = document.querySelector('.texto-descricao');
+const sobreSkill = [
+                    '<p><Strong>HTML</Strong></p> <br> <p>É uma linguagem de marcação utilizada na construção de páginas na Web.</p> <br>',
+                    '<p><Strong>CSS</Strong></p> <br> <p>É um mecanismo para adicionar estilo a um documento web.</p> <br>',
+                    '<p><Strong>JavaScript</Strong></p> <br> <p>É uma linguagem de programação. Juntamente com HTML e CSS, é uma das três principais tecnologias da web.</p> <br>',
+                    '<p><Strong>Git</Strong></p> <br> <p>É um sistema de controle de versões distribuído, usado principalmente no desenvolvimento de software.</p> <br>',
+                    '<p><Strong>PHP</Strong></p> <br> <p>O PHP é uma linguagem de programação voltada originalmente para desenvolvimento de sites e aplicações web.</p> <br>',
+                    '<p><Strong>Bootstrap</Strong></p> <br> <p>Bootstrap é um framework web com código-fonte aberto para desenvolvimento de componentes de interface e front-end para sites e aplicações web</p> <br>'
+                    
+]
+const dataInicio = [
+                    (new Date(2021, 10, 1)).getTime(), // 0
+                    (new Date(2021, 10, 1)).getTime(), // 1
+                    (new Date(2022, 0, 1)).getTime(), // 2
+                    (new Date(2022, 3, 1)).getTime(), // 3
+                    (new Date(2022, 3, 1)).getTime(), // 4
+                    (new Date(2022, 1, 1)).getTime(), // 5
+                    (new Date(2021, 11, 1)).getTime(), // 6
+                    (new Date(2021, 11, 1)).getTime(), // 7
+                    ]
+const dataHoje = (new Date()).getTime();
+
+skills.forEach(  (elemento, index) => {
+    let index1 = index;
+    let elemento1 = elemento;
+    elemento.addEventListener('mouseover', (evento) => {
+        let tempo = 'mês';
+        let tempoDeExperiencia = Math.round((dataHoje-dataInicio[index1])/(1000*60*60*24*30));
+        if (tempoDeExperiencia > 1) tempo = 'meses'
+        descricao.innerHTML = `<p>${sobreSkill[index1]} </p>` ;
+    } )
+    elemento.addEventListener('mouseout', (evento, elemento,) => {
+        descricao.innerHTML = '/* Passe o mouse por cima de alguma habilidade para ler a descrição */';
+    } )
+} );
+
+
